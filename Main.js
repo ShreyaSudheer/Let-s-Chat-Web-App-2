d5fb0f0e919f5e0e9f4e9d5825fd02be2738a90e
Shreya <87652405+ShreyaSudheer@users.noreply.github.com>
2021-12-08 00:00:16 -0800

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,13 @@
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBFxrOMB7Nf2oy3QFtze588jkliJMa5t-g",
+    authDomain: "let-s-chat-web-app-fbab5.firebaseapp.com",
+    projectId: "let-s-chat-web-app-fbab5",
+    storageBucket: "let-s-chat-web-app-fbab5.appspot.com",
+    messagingSenderId: "694569303764",
+    appId: "1:694569303764:web:9f4b286de58917f2170541"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
